4500a066c6eccb18f9cc6fba9a59dc246d5e3044
xfgu <632167902@qq.com>
2026-01-18 17:33:42 +0800

    feat: add 404 not found page

---
+++ src/views/404.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="not-found">
+    <h1>404</h1>
+    <p>页面不存在</p>
+    <el-button type="primary" @click="goHome">返回首页</el-button>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const goHome = () => {
+  router.push('/')
+}
+</script>
+
+<style scoped>
+.not-found {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background: #f5f5f5;
+}
+
+h1 {
+  font-size: 120px;
+  color: #409eff;
+  margin: 0 0 20px 0;
+}
+
+p {
+  font-size: 20px;
+  color: #666;
+  margin-bottom: 30px;
+}
+</style>
